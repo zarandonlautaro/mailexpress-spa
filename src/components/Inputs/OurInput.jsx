d1cd5d31c73ccd94937e378e05505201f927877c
zarandonlautaro <zarandonlautaro@mail.com>
2020-09-20 18:30:50 -0300

    feature! : modularize inputs

---
+++ src/components/Inputs/OurInput.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import {
+  Row, Col, FormGroup, Label, Input, FormText,
+} from 'reactstrap';
+export default class OurInput extends Component {
+  state = {
+    onChange: this.props.onChange,
+    onBlur: this.props.onBlur,
+    onKeyPress: this.props.handleKeyPress,
+  }
+  render() {
+    const { label, type, name, placeholder, error } = this.props;
+    const { onChange, onBlur } = this.state;
+    return (
+      <Row>
+        <Col>
+          <FormGroup>
+            <Label>{label}</Label>
+            <Input
+              type={type}
+              name={name}
+              placeholder={placeholder}
+              onChange={onChange}
+              onBlur={onBlur}
+            />
+            {
+              error
+              && (
+                <FormText color="danger">
+                  {error}
+                </FormText>
+              )
+            }
+          </FormGroup>
+        </Col>
+      </Row>
+    )
+  }
+}
